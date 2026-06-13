df6b42af9842550864ae655bb23b9970f9b609e3
dima <dmytroperets@gmail.com>
2026-06-13 15:44:56 +0200

    Simplified the code; Increased cache file size limit to 64MiB. Added more comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 #include "return_codes.h"
 #include "lib.cpp"
 
+
+void copy_argument(char* dest, const char* src, int len)
+{
+    dest = new char[len + 1];
+    strncpy(dest, src, len);
+    dest[len] = 0;
+}
+
+
 int main(int argc, char** argv)
 {
     if (argc < 2) // If we have arguments
@@ -16,30 +25,28 @@ int main(int argc, char** argv)
 
     Log::Set_LogLevel(MainConfig::Instance().get_log_level());
 
-    char** args = new char*[argc];
+    char** args = new char*[argc]; // A copy of script's arguments
 
-    for (int i = 1; i < argc; ++i)
-    {
-        int len = strlen(argv[i]);
-        args[i - 1] = new char[len + 1];
-        strncpy(args[i - 1], argv[i], len);
-        args[i - 1][len] = 0;
-    }
+    for (int i = 1; i < argc; ++i) // Copy each argument
+        copy_argument(args[i - 1], argv[i], strlen(argv[i]));
 
-    args[argc - 1] = nullptr;
+    args[argc - 1] = nullptr; // Last argument should be NULL
 
-    std::string output = compile(args[0]);
+    std::string output = compile(args[0]); // Compile the script
 
+    // Free initial string
     delete[] args[0];
-    args[0] = new char[output.length() + 1];
-    strncpy(args[0], output.c_str(), output.length());
-    args[0][output.length()] = '\0';
 
+    // Replace it with compiled binary name
+    copy_argument(args[0], output.c_str(), output.length());
+
+    // Run the binary
     run(args[0], args);
 
+    // If the binary is bigger than 64MiB don't keep it in cache
     struct stat st{ };
-    if (::stat(args[0], &st) == 0 && st.st_size >= 32 * 1024 * 1024)
+    if (::stat(args[0], &st) == 0 && st.st_size >= 64 * 1024 * 1024)
         rm(args[0]); // Remove large files
 
-    return ERROR_OK;
+    return ERROR_OK; // Exit Successfully
 }
